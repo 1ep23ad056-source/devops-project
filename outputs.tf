eceb49bd89e01150271f8f489eb8a5cc97c673ae
maha <swathi123@gmail.com>
2026-07-09 10:47:03 +0530

    initial terraform project for food delivery application

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.food_rg.name
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.food_rg.location
+}
+
+output "resource_group_id" {
+  value = azurerm_resource_group.food_rg.id
+}
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+subscription_id = "c74aa966-f6ae-4b6d-9686-7e8c85cbb3ab"
+
+resource_group_name = "FoodDelivery-RG"
+
+location = "Central India"
